369c49f9951a39f0d644bb126c15e90bbe56db47
Bozhidar Zashev <bzashev@users.noreply.github.com>
2024-06-20 17:58:09 +0300

    dev

--- src/WebVella.Tefter.Web/Models/ThemeSettings.cs
+++ src/WebVella.Tefter.Web/Models/ThemeSettings.cs
@@ -4,6 +4,15 @@ namespace WebVella.Tefter.Web.Models;
 
 public record ThemeSettings
 {
+	[JsonIgnore]
 	public DesignThemeModes ThemeMode { get; init; } = DesignThemeModes.System;
+	
+	[JsonPropertyName("mode")]
+	public string Mode { get => ThemeMode.ToString(); }
+
+	[JsonIgnore]
 	public OfficeColor ThemeColor { get; init; } = OfficeColor.Excel;
+
+	[JsonPropertyName("color")]
+	public string Color { get => ThemeColor.ToString(); }
 }
