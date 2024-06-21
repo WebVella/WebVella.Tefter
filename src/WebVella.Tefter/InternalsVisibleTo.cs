be882b13168d6a4cfbed471e5467e50fcdbd6060
Rumen Yankov <rumen.yankov@gmail.com>
2024-06-21 09:21:44 +0300

    impl

--- src/WebVella.Tefter/InternalsVisibleTo.cs
+++ src/WebVella.Tefter/InternalsVisibleTo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("WebVella.Tefter.Tests")]
+[assembly: InternalsVisibleTo("WebVella.Tefter.Web")]
 //[assembly: InternalsVisibleTo("WebVella.Tefter.Core")]
