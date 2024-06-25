562bd08a0388acb9bdfd5542fc8a5ed1365930c9
Rumen Yankov <rumen.yankov@gmail.com>
2024-06-25 17:37:21 +0300

    changes InternalsVisibleTo

--- src/WebVella.Tefter/InternalsVisibleTo.cs
+++ src/WebVella.Tefter/InternalsVisibleTo.cs
@@ -1,5 +1,3 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("WebVella.Tefter.Tests")]
-[assembly: InternalsVisibleTo("WebVella.Tefter.Web")]
-//[assembly: InternalsVisibleTo("WebVella.Tefter.Core")]
