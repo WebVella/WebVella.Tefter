694414c4efe90bcc647fe65b8a842317ff640f1e
Rumen Yankov <rumen.yankov@gmail.com>
2024-06-28 20:58:46 +0300

    join migrations - new databases will be created

--- src/WebVella.Tefter/Migrations/TefterSystemMigration.2024062701.cs
+++ src/WebVella.Tefter/Migrations/TefterSystemMigration.2024062701.cs
@@ -163,7 +163,35 @@ internal class TefterSystemMigration2024062701 : TefterSystemMigration
 						.WithForeignTable("data_provider")
 						.WithForeignColumns("id");
 					});
-			}); 
+			});
+		#endregion
+
+		#region  TABLE: DATA_PROVIDER_SHARED_KEY
+		dbBuilder
+			.NewTableBuilder(Guid.NewGuid(), "data_provider_shared_key")
+			.WithColumns(columns =>
+			{
+				columns
+					.AddGuidColumn("id", c => { c.NotNullable().WithAutoDefaultValue(); })
+					.AddGuidColumn("data_provider_id", c => { c.NotNullable().WithoutAutoDefaultValue(); })
+					.AddShortTextColumn("db_name", c => { c.NotNullable(); })
+					.AddShortTextColumn("description", c => { c.NotNullable().WithDefaultValue(string.Empty); })
+					.AddTextColumn("column_ids_json", c => { c.NotNullable().WithDefaultValue("[]"); })
+					.AddShortIntegerColumn("version", c => { c.NotNullable().WithDefaultValue(1); })
+					.AddDateTimeColumn("last_modified_on", c => { c.NotNullable().WithAutoDefaultValue(); });
+			})
+			.WithConstraints(constraints =>
+			{
+				constraints
+					.AddPrimaryKeyConstraint("pk_data_provider_shared_key", c => { c.WithColumns("id"); })
+					.AddForeignKeyConstraint("fk_data_provider_shared_key_data_provider", c =>
+					{
+						c.WithColumns("data_provider_id")
+						.WithForeignTable("data_provider")
+						.WithForeignColumns("id");
+					});
+			});
+
 		#endregion
 	}
 
