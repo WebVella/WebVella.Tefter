e4c7003f31de19c4ead966cef1d543a567b7c859
Bozhidar Zashev <bzashev@users.noreply.github.com>
2024-07-14 09:07:19 +0300

    nuget update

--- src/WebVella.Tefter/UseCases/UserAdmin/UserAdminUseCase.cs
+++ src/WebVella.Tefter/UseCases/UserAdmin/UserAdminUseCase.cs
@@ -45,4 +45,52 @@ public partial class UserAdminUseCase
 	}
 
 
+	internal async Task<Result<TucUser>> CreateUserWithFormAsync()
+	{
+		UserBuilder userBuilder = _identityManager.CreateUserBuilder(null);
+		userBuilder
+			.WithEmail(Form.Email)
+			.WithFirstName(Form.FirstName)
+			.WithLastName(Form.LastName)
+			.WithPassword(Form.Password)
+			.Enabled(Form.Enabled)
+			.CreatedOn(DateTime.Now)
+			.WithThemeMode(Form.ThemeMode)
+			.WithThemeColor(Form.ThemeColor)
+			.WithOpenSidebar(true)
+			.WithCultureCode(Form.Culture.CultureInfo.Name)
+			.WithRoles(Form.Roles.Select(x => x.ToModel()).ToArray());
+
+		var user = userBuilder.Build();
+		var result = await _identityManager.SaveUserAsync(user);
+		if (result.IsFailed) return Result.Fail(new Error("SaveUserAsync failed").CausedBy(result.Errors));
+		
+		return Result.Ok(new TucUser(result.Value));
+	}
+
+	internal async Task<Result<TucUser>> UpdateUserWithFormAsync()
+	{
+		var currentUserResult = await _identityManager.GetUserAsync(Form.Id);
+		if (currentUserResult.IsFailed) return Result.Fail(new Error("GetUserAsync failed").CausedBy(currentUserResult.Errors));
+		if (currentUserResult.Value is null) return Result.Fail(new Error("GetUserAsync - no user was created"));
+
+		UserBuilder userBuilder = _identityManager.CreateUserBuilder(currentUserResult.Value);
+		userBuilder
+			.WithEmail(Form.Email)
+			.WithFirstName(Form.FirstName)
+			.WithLastName(Form.LastName)
+			.Enabled(Form.Enabled)
+			.WithThemeMode(Form.ThemeMode)
+			.WithThemeColor(Form.ThemeColor)
+			.WithCultureCode(Form.Culture.CultureInfo.Name)
+			.WithRoles(Form.Roles.Select(x => x.ToModel()).ToArray());
+
+		if (!String.IsNullOrWhiteSpace(Form.Password))
+			userBuilder.WithPassword(Form.Password);
+
+		var user = userBuilder.Build();
+		var result = await _identityManager.SaveUserAsync(user);
+		if (result.IsFailed) return Result.Fail(new Error("SaveUserAsync failed").CausedBy(result.Errors));
+		return Result.Ok(new TucUser(result.Value));
+	}
 }
