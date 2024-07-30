9c9408acc592c10384497c77b0bb8187639398b6
Rumen Yankov <rumen.yankov@gmail.com>
2024-07-30 11:00:02 +0300

    impl

--- src/WebVella.Tefter/Models/TfDataRow.cs
+++ src/WebVella.Tefter/Models/TfDataRow.cs
@@ -32,8 +32,54 @@ public class TfDataRow : IEnumerable
 		this.DataTable = dataTable;
 	}
 
+	public T Field<T>(string columnName)
+	{
+		return UnboxT<T>.Unbox(this[columnName]);
+	}
+
 	public IEnumerator GetEnumerator()
 	{
 		return _values.GetEnumerator();
 	}
+
+	private static class UnboxT<T>
+	{
+		internal static readonly Converter<object, T> Unbox = Create(typeof(T));
+
+		private static Converter<object, T> Create(Type type)
+		{
+			if (type.IsValueType)
+			{
+				if (type.IsGenericType && !type.IsGenericTypeDefinition && (typeof(Nullable<>) == type.GetGenericTypeDefinition()))
+				{
+					return (Converter<object, T>)Delegate.CreateDelegate(
+						typeof(Converter<object, T>),
+							typeof(UnboxT<T>)
+								.GetMethod("NullableField", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic)
+								.MakeGenericMethod(type.GetGenericArguments()[0]));
+				}
+				return ValueField;
+			}
+			return ReferenceField;
+		}
+
+		private static T ReferenceField(object value)
+		{
+			return ((DBNull.Value == value) ? default(T) : (T)value);
+		}
+
+		private static T ValueField(object value)
+		{
+			return (T)value;
+		}
+
+		private static Nullable<TElem> NullableField<TElem>(object value) where TElem : struct
+		{
+			if (value is null)
+				return default(Nullable<TElem>);
+
+			return new Nullable<TElem>((TElem)value);
+		}
+	}
+
 }
