50ab1dbee52c729bc3ce09d7c7472a0a99c3f363
Rumen Yankov <rumen.yankov@gmail.com>
2024-08-30 13:24:50 +0300

    fix

--- src/WebVella.Tefter/Models/TfFilterBase.cs
+++ src/WebVella.Tefter/Models/TfFilterBase.cs
@@ -1,12 +1,12 @@
 ﻿namespace WebVella.Tefter;
 
-[JsonDerivedType(typeof(TfFilterBoolean), typeDiscriminator: "text")]
-[JsonDerivedType(typeof(TfFilterDateOnly), typeDiscriminator: "text")]
-[JsonDerivedType(typeof(TfFilterDateTime), typeDiscriminator: "text")]
-[JsonDerivedType(typeof(TfFilterGuid), typeDiscriminator: "text")]
-[JsonDerivedType(typeof(TfFilterNumeric), typeDiscriminator: "text")]
-[JsonDerivedType(typeof(TfFilterOr), typeDiscriminator: "text")]
-[JsonDerivedType(typeof(TfFilterAnd), typeDiscriminator: "text")]
+[JsonDerivedType(typeof(TfFilterBoolean), typeDiscriminator: "boolean")]
+[JsonDerivedType(typeof(TfFilterDateOnly), typeDiscriminator: "dateonly")]
+[JsonDerivedType(typeof(TfFilterDateTime), typeDiscriminator: "datetime")]
+[JsonDerivedType(typeof(TfFilterGuid), typeDiscriminator: "guid")]
+[JsonDerivedType(typeof(TfFilterNumeric), typeDiscriminator: "numeric")]
+[JsonDerivedType(typeof(TfFilterOr), typeDiscriminator: "or")]
+[JsonDerivedType(typeof(TfFilterAnd), typeDiscriminator: "and")]
 [JsonDerivedType(typeof(TfFilterText), typeDiscriminator: "text")]
 public abstract class TfFilterBase
 {
