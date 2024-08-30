76faa299625410aa73ba913b88faaf0facfda64c
Rumen Yankov <rumen.yankov@gmail.com>
2024-08-30 13:16:55 +0300

    fixes filter serialization

--- src/WebVella.Tefter/Models/TfFilterBase.cs
+++ src/WebVella.Tefter/Models/TfFilterBase.cs
@@ -1,5 +1,13 @@
 ﻿namespace WebVella.Tefter;
 
+[JsonDerivedType(typeof(TfFilterBoolean), typeDiscriminator: "text")]
+[JsonDerivedType(typeof(TfFilterDateOnly), typeDiscriminator: "text")]
+[JsonDerivedType(typeof(TfFilterDateTime), typeDiscriminator: "text")]
+[JsonDerivedType(typeof(TfFilterGuid), typeDiscriminator: "text")]
+[JsonDerivedType(typeof(TfFilterNumeric), typeDiscriminator: "text")]
+[JsonDerivedType(typeof(TfFilterOr), typeDiscriminator: "text")]
+[JsonDerivedType(typeof(TfFilterAnd), typeDiscriminator: "text")]
+[JsonDerivedType(typeof(TfFilterText), typeDiscriminator: "text")]
 public abstract class TfFilterBase
 {
 	public string ColumnName { get; init; }
