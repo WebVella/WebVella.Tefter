5c1f786bb15c270416d40edc4426a1e945f6be51
Bozhidar Zashev <bzashev@users.noreply.github.com>
2024-08-30 15:47:39 +0300

    dev

--- src/WebVella.Tefter/Web/Components/FilterManage/TfFilterManage.razor.cs
+++ src/WebVella.Tefter/Web/Components/FilterManage/TfFilterManage.razor.cs
@@ -7,10 +7,10 @@ public partial class TfFilterManage : TfBaseComponent
 	public TfSpaceDataManage TfSpaceDataManage { get; set; }
 
 	[Parameter]
-	public TucFilterBase Item { get; set;}
+	public TucFilterBase Item { get; set; }
 
 	[Parameter]
-	public bool Disabled { get; set;} = false;
+	public bool Disabled { get; set; } = false;
 
 	private string _selectedFilterColumn = null;
 
@@ -27,6 +27,139 @@ public partial class TfFilterManage : TfBaseComponent
 		await TfSpaceDataManage.RemoveColumnFilter(Item.Id);
 	}
 
+	private async Task _valueChanged(TucFilterBase model, string propName, object valueObj)
+	{
+		if (model is TucFilterAnd || model is TucFilterOr) return;
+		TucFilterBase updateObj = null;
+		if (model is TucFilterBoolean)
+		{
+			var original = (TucFilterBoolean)model with {Id = model.Id};
+			var item = (TucFilterBoolean)model with {Id = model.Id};
+			if (propName == nameof(item.ComparisonMethod))
+			{
+				var value = (TucFilterBooleanComparisonMethod)valueObj;
+				item.ComparisonMethod = value;
+				if (item.ComparisonMethod == original.ComparisonMethod) return;
+				updateObj = item;
+			}
+			else if (propName == nameof(item.Value))
+			{
+				var value = (Option<string>)valueObj;
+				item.ValueOptionChanged(value);
+				if (item.Value == original.Value) return;
+				updateObj = item;
+			}
+			else throw new Exception("propName not supported");
+		}
+		else if (model is TucFilterDateOnly)
+		{
+			var original = (TucFilterDateOnly)model with {Id = model.Id};
+			var item = (TucFilterDateOnly)model with {Id = model.Id};
+			if (propName == nameof(item.ComparisonMethod))
+			{
+				var value = (TucFilterDateTimeComparisonMethod)valueObj;
+				item.ComparisonMethod = value;
+				if (item.ComparisonMethod == original.ComparisonMethod) return;
+				updateObj = item;
+			}
+			else if (propName == nameof(item.Value))
+			{
+				var value = (string)valueObj;
+				item.ValueStringChanged(value);
+				if (item.Value == original.Value) return;
+				updateObj = item;
+			}
+			else throw new Exception("propName not supported");
+		}
+		else if (model is TucFilterDateTime)
+		{
+			var original = (TucFilterDateTime)model with {Id = model.Id};
+			var item = (TucFilterDateTime)model with {Id = model.Id};
+			if (propName == nameof(item.ComparisonMethod))
+			{
+				var value = (TucFilterDateTimeComparisonMethod)valueObj;
+				item.ComparisonMethod = value;
+				if (item.ComparisonMethod == original.ComparisonMethod) return;
+				updateObj = item;
+			}
+			else if (propName == nameof(item.Value))
+			{
+				var value = (string)valueObj;
+				item.ValueStringChanged(value);
+				if (item.Value == original.Value) return;
+				updateObj = item;
+			}
+			else throw new Exception("propName not supported");
+		}
+		else if (model is TucFilterGuid)
+		{
+			var original = (TucFilterGuid)model with {Id = model.Id};
+			var item = (TucFilterGuid)model with {Id = model.Id};
+			if (propName == nameof(item.ComparisonMethod))
+			{
+				var value = (TucFilterGuidComparisonMethod)valueObj;
+				item.ComparisonMethod = value;
+				if (item.ComparisonMethod == original.ComparisonMethod) return;
+				updateObj = item;
+			}
+			else if (propName == nameof(item.Value))
+			{
+				var value = (string)valueObj;
+				if (!String.IsNullOrWhiteSpace(value) && !Guid.TryParse(value, out Guid outGuid))
+					ToastService.ShowError(LOC("Invalid GUID value"));
+
+				item.ValueStringChanged(value);
+				if (item.Value == original.Value) return;
+				updateObj = item;
+			}
+			else throw new Exception("propName not supported");
+		}
+		else if (model is TucFilterNumeric)
+		{
+			var original = (TucFilterNumeric)model with {Id = model.Id};
+			var item = (TucFilterNumeric)model with {Id = model.Id};
+			if (propName == nameof(item.ComparisonMethod))
+			{
+				var value = (TucFilterNumericComparisonMethod)valueObj;
+				updateObj = item with { ComparisonMethod = value };
+				item.ComparisonMethod = value;
+				if (item.ComparisonMethod == original.ComparisonMethod) return;
+				updateObj = item;
+			}
+			else if (propName == nameof(item.Value))
+			{
+				var value = (decimal?)valueObj;
+				item.ValueChanged(value);
+				if (item.Value == original.Value) return;
+				updateObj = item;
+			}
+			else throw new Exception("propName not supported");
+		}
+		else if (model is TucFilterText)
+		{
+			var original = (TucFilterText)model with {Id = model.Id};
+			var item = (TucFilterText)model with {Id = model.Id};
+			if (propName == nameof(item.ComparisonMethod))
+			{
+				var value = (TucFilterTextComparisonMethod)valueObj;
+				item.ComparisonMethod = value;
+				if (item.ComparisonMethod == original.ComparisonMethod) return;
+				updateObj = item;
+			}
+			else if (propName == nameof(item.Value))
+			{
+				var value = (string)valueObj;
+				item.ValueChanged(value);
+				if (item.Value == original.Value) return;
+				updateObj = item;
+			}
+			else throw new Exception("propName not supported");
+		}
+		else throw new Exception("Unsupported TucFilterBase in _valueChanged");
+
+		await TfSpaceDataManage.UpdateColumnFilter(updateObj);
+		await InvokeAsync(StateHasChanged);
+	}
 
 
 }
