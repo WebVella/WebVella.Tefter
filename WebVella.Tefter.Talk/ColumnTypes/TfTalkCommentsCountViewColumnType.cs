2f2146730c0c4975803668365ea44abfc2333781
Bozhidar Zashev <bzashev@users.noreply.github.com>
2024-09-24 18:39:14 +0300

    app component

---
+++ WebVella.Tefter.Talk/ColumnTypes/TfTalkCommentsCountViewColumnType.cs
@@ -0,0 +1,44 @@
+﻿namespace WebVella.Tefter.Web.ViewColumns;
+
+public class TfTalkCommentsCountViewColumnType : ITfSpaceViewColumnType
+{
+	public Guid Id { get; init; }
+	public string Name { get; init; }
+	public string Description { get; init; }
+	public string Icon { get; init; }
+	public List<TfSpaceViewColumnDataMapping> DataMapping { get; init; }
+	public Type DefaultComponentType { get; init; }
+	public Type CustomOptionsComponentType { get; init; }
+	public List<Type> SupportedComponentTypes { get; init; }
+	public List<string> FilterAliases { get; init; }
+	public List<string> SortAliases { get; init; }
+	public List<Guid> SupportedAddonTypes { get; init; } = new();
+
+
+	public TfTalkCommentsCountViewColumnType()
+	{
+
+		Id = new Guid("60ab4197-be46-4ebd-a6de-02e8d25450d3");
+
+		Name = "Talk Comments count";
+
+		Description = "displays related comments count";
+
+		Icon = "TextCaseTitle";
+
+		DataMapping = new List<TfSpaceViewColumnDataMapping>
+		{
+		};
+
+		FilterAliases = new List<string>() { };
+
+		SortAliases = new List<string> { };
+
+		DefaultComponentType = typeof(TfTalkCommentsCountComponent);
+
+		SupportedComponentTypes = new List<Type> {
+			typeof(TfTalkCommentsCountComponent)
+			};
+	}
+}
+
--- WebVella.Tefter.Talk/Global.cs
+++ WebVella.Tefter.Talk/Global.cs
@@ -1,6 +1,10 @@
-﻿
-/// Components
+﻿global using System.ComponentModel;
+global using Microsoft.FluentUI.AspNetCore.Components;
+
+global using WebVella.Tefter.Models;
 global using WebVella.Tefter.Web.Components;
 global using WebVella.Tefter.Web.Models;
 global using WebVella.Tefter.Web.Services;
 global using WebVella.Tefter.Web.Utils;
+
+global using WebVella.Tefter.Talk.Components;
