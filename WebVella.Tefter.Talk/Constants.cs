4adddf8b645728c2a823c48c41968238d8519a6d
Bozhidar Zashev <bzashev@users.noreply.github.com>
2024-09-24 16:57:04 +0300

    adding talk app

---
+++ WebVella.Tefter.Talk/Constants.cs
@@ -0,0 +1,5 @@
+﻿namespace WebVella.Tefter.Talk;
+internal class Constants
+{
+	public static Guid TALK_APP_ID = new Guid("27a7703a-8fe8-4363-aee1-64a219d7520e");
+}
---
+++ WebVella.Tefter.Talk/Global.cs
@@ -0,0 +1,6 @@
+﻿
+/// Components
+global using WebVella.Tefter.Web.Components;
+global using WebVella.Tefter.Web.Models;
+global using WebVella.Tefter.Web.Services;
+global using WebVella.Tefter.Web.Utils;
