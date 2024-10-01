c540c865182e7bc6351dc0cf22c6c4097d463c0e
Bozhidar Zashev <bzashev@users.noreply.github.com>
2024-10-01 18:19:19 +0300

    dev

---
+++ src/WebVella.Tefter/Web/Components/General/Editor/TfEditor.razor.cs
@@ -0,0 +1,43 @@
+﻿namespace WebVella.Tefter.Web.Components;
+[LocalizationResource("WebVella.Tefter.Web.Components.General.Editor.TfEditor", "WebVella.Tefter")]
+public partial class TfEditor : TfBaseComponent
+{
+	[Parameter] public string Value { get; set; }
+	[Parameter] public EventCallback<string> ValueChanged { get; set; }
+
+	private CancellationTokenSource inputThrottleCancalationToken = new();
+
+	private Guid _componentId = Guid.NewGuid();
+
+	private string strSavedContent = "";
+	private ElementReference divEditorElement;
+	private string EditorContent;
+	private string EditorHTMLContent;
+	private bool EditorEnabled = true;
+
+	protected override async Task OnInitializedAsync()
+	{
+		await base.OnInitializedAsync();
+	}
+
+	protected override async Task OnAfterRenderAsync(bool firstRender)
+	{
+		if (firstRender)
+		{
+			await JSRuntime.InvokeAsync<string>(
+				"Tefter.createQuill", divEditorElement, _componentId.ToString());
+		}
+	}
+
+	private async Task _valueChanged(string value)
+	{
+		inputThrottleCancalationToken.Cancel();
+		inputThrottleCancalationToken = new();
+		await Task.Delay(1000, inputThrottleCancalationToken.Token).ContinueWith(
+		async (task) =>
+		{
+			await ValueChanged.InvokeAsync(value);
+		}, inputThrottleCancalationToken.Token);
+	}
+
+}
--- src/WebVella.Tefter/wwwroot/app.js
+++ src/WebVella.Tefter/wwwroot/app.js
@@ -1,23 +1,83 @@
 ﻿window.Tefter = {
-	copyToClipboard: function(text) {
-        navigator.clipboard.writeText(text).then(function () {})
-        .catch(function (error) {
-            log.error(error);
-        });
-    },
-	clickElement: function(elementId) {
-        var element = document.getElementById(elementId);
-        if(!element) return;
-        element.click();
-    },
-	focusElement: function(elementId) {
-        var element = document.getElementById(elementId);
-        if(!element) return;
-        element.focus();
-    },
-	blurElement: function(elementId) {
-        var element = document.getElementById(elementId);
-        if(!element) return;
-        element.blur();
-    }
+	HtmlEditors: {},
+	HtmlEditorsChangeListeners: {},
+	dispose: function () {
+		Tefter.HtmlEditors = {};
+		Tefter.HtmlEditorsChangeListeners = {};
+	},
+	copyToClipboard: function (text) {
+		navigator.clipboard.writeText(text).then(function () { })
+			.catch(function (error) {
+				log.error(error);
+			});
+	},
+	clickElement: function (elementId) {
+		var element = document.getElementById(elementId);
+		if (!element) return;
+		element.click();
+	},
+	focusElement: function (elementId) {
+		var element = document.getElementById(elementId);
+		if (!element) return;
+		element.focus();
+	},
+	blurElement: function (elementId) {
+		var element = document.getElementById(elementId);
+		if (!element) return;
+		element.blur();
+	},
+	createQuill: function (quillElement, editorId) {
+		var options = {
+			modules: {
+				toolbar: {
+					container: '#toolbar-' + editorId,
+					handlers: {
+						'undo': () => Tefter.HtmlEditors[editorId].history.undo(),
+						'redo': () => Tefter.HtmlEditors[editorId].history.redo(),
+					},
+				},
+				history: {
+					delay: 2000,
+					maxStack: 500,
+					userOnly: true
+				},
+
+			},
+			placeholder: '',
+			readOnly: false
+		};
+		// set quill at the object we can call
+		// methods on later
+		Tefter.HtmlEditors[editorId] = new Quill(quillElement, options);
+		Tefter.HtmlEditors[editorId].on('text-change', (delta, oldDelta, source) => {
+			if (source == 'api') {
+				console.log('An API call triggered this change.');
+				Tefter.executeEditorTextChangeListenerCallbacks(delta, oldDelta, source)
+			} else if (source == 'user') {
+				console.log('A user action triggered this change.');
+			}
+		});
+	},
+	addEditorTextChangeListener: function (dotNetHelper, editorId, methodName) {
+		Tefter.HtmlEditorsChangeListeners[listenerId] = { dotNetHelper: dotNetHelper, methodName: methodName };
+		return true;
+	},
+	removeEditorTextChangeListener: function (dotNetHelper, editorId, methodName) {
+		if (Tefter.HtmlEditorsChangeListeners[listenerId]) {
+			delete Tefter.HtmlEditorsChangeListeners[listenerId];
+		}
+		return true;
+	},
+	executeEditorTextChangeListenerCallbacks: function (delta, oldDelta, source) {
+		if (Tefter.HtmlEditorsChangeListeners) {
+			for (const prop in Tefter.HtmlEditorsChangeListeners) {
+				const dotNetHelper = Tefter.HtmlEditorsChangeListeners[prop].dotNetHelper;
+				const methodName = Tefter.HtmlEditorsChangeListeners[prop].methodName;
+				if (dotNetHelper && methodName) {
+					dotNetHelper.invokeMethodAsync(methodName);
+				}
+			}
+		}
+		return true;
+	},
 }
