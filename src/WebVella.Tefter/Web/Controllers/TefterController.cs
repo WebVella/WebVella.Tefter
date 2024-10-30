57fa215f356a280f7465954b318b5b139d8ccafc
Rumen Yankov <rumen.yankov@gmail.com>
2024-10-30 17:29:34 +0200

    impl

--- src/WebVella.Tefter/Web/Controllers/TefterController.cs
+++ src/WebVella.Tefter/Web/Controllers/TefterController.cs
@@ -2,19 +2,27 @@
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+using System.Net;
+
 [Route("api")]
 [ResponseCache(Location = ResponseCacheLocation.None, Duration = 0, NoStore = true)]
 public class ApiExcelController : ControllerBase
 {
+	private readonly ITfFileManager _fileManager;
 	internal readonly ExportUseCase UC;
 	public ApiExcelController(
-		ExportUseCase uc)
+		ExportUseCase uc,
+		ITfFileManager fileManager)
 	{
 		UC = uc;
+		_fileManager = fileManager;
 	}
 
 	[Route("export/{exportType}")]
-	[HttpPost,HttpGet]
+	[HttpPost, HttpGet]
 	public async Task<ActionResult> ExportHander([FromRoute] string exportType)
 	{
 		try
@@ -69,6 +77,56 @@ public class ApiExcelController : ControllerBase
 			};
 		}
 	}
+
+
+	[HttpGet]
+	[Route("/fs/{*filePath}")]
+	public IActionResult Download([FromRoute] string filePath)
+	{
+		if (string.IsNullOrWhiteSpace(filePath))
+		{
+			HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+			return new JsonResult(new { });
+		}
+
+
+		var file = _fileManager.FindFile(filePath).Value;
+
+		if (file == null)
+		{
+			HttpContext.Response.StatusCode = (int)HttpStatusCode.NotFound;
+			return new JsonResult(new { });
+		}
+		
+		string headerModifiedSince = Request.Headers["If-Modified-Since"];
+
+		if (headerModifiedSince != null)
+		{
+			if (DateTime.TryParse(headerModifiedSince, out DateTime isModifiedSince))
+			{
+				if (isModifiedSince <= file.LastModifiedOn)
+				{
+					Response.StatusCode = 304;
+					return new EmptyResult();
+				}
+			}
+		}
+		var cultureInfo = new CultureInfo("en-US");
+		
+		HttpContext.Response.Headers.Add("last-modified", file.LastModifiedOn.ToString(cultureInfo));
+		
+		const int durationInSeconds = 60 * 60 * 24 * 30; //30 days caching of these resources
+		
+		HttpContext.Response.Headers[HeaderNames.CacheControl] = "public,max-age=" + durationInSeconds;
+
+		var extension = Path.GetExtension(filePath).ToLowerInvariant();
+		
+		new FileExtensionContentTypeProvider().Mappings.TryGetValue(extension, out string mimeType);
+
+		Stream fileContentStream = _fileManager.GetFileContentAsFileStream(file).Value;
+		return File( fileContentStream, mimeType);
+	}
+
 
 
 }
