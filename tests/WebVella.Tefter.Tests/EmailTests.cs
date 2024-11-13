29d0442b8193952e822f82c661afcc4756c6960a
Rumen Yankov <rumen.yankov@gmail.com>
2024-11-13 15:39:48 +0200

    restructure

---
+++ tests/WebVella.Tefter.Tests/EmailTests.cs
@@ -0,0 +1,28 @@
+﻿namespace WebVella.Tefter.Tests;
+
+using WebVella.Tefter.Email.Models;
+using WebVella.Tefter.Email.Services;
+
+
+public partial class TalkTests : BaseTest
+{
+	[Fact]
+	public async Task Email_CRUD()
+	{
+		using (await locker.LockAsync())
+		{
+			ITfDatabaseService dbService = ServiceProvider.GetRequiredService<ITfDatabaseService>();
+			IEmailService talkService = ServiceProvider.GetRequiredService<IEmailService>();
+			IIdentityManager identityManager = ServiceProvider.GetRequiredService<IIdentityManager>();
+			ITfDataManager dataManager = ServiceProvider.GetRequiredService<ITfDataManager>();
+
+
+			using (var scope = dbService.CreateTransactionScope(Constants.DB_OPERATION_LOCK_KEY))
+			{
+				var user = identityManager.GetUser("rumen@webvella.com").Value;
+
+			}
+		}
+	}
+
+}
