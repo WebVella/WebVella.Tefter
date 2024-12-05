4c312e4257f5245e5ef811548b38747cf0a3d886
Rumen Yankov <rumen.yankov@gmail.com>
2024-12-05 09:58:55 +0200

    changes assets folder shared_key column to nullable in database

---
+++ src/WebVella.Tefter.Assets/Migrations/AssetsMigration2024120501.cs
@@ -0,0 +1,23 @@
+﻿namespace WebVella.Tefter.Assets.Migrations;
+
+
+[TfApplicationMigration(TfAssetsConstants.ASSETS_APP_ID_STRING, "2024.12.5.1")]
+public class AssetsMigration2024120501 : ITfApplicationMigration
+{
+	public async Task MigrateDataAsync(ITfApplication app, IServiceProvider serviceprovider, ITfDatabaseService dbService)
+	{
+		await Task.Delay(0);
+	}
+
+	public async Task MigrateStructureAsync(ITfApplication app, TfDatabaseBuilder dbBuilder)
+	{
+		await Task.Delay(0);
+
+		dbBuilder
+			.WithTableBuilder("assets_folder")
+			.WithColumns(columns =>
+			{
+				columns.WithShortTextColumn("shared_key", c => c.Nullable());
+			});
+	}
+}
