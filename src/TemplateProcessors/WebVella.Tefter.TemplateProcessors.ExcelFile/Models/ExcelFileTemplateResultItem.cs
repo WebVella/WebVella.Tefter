882736f8008cd928cc46beb45d190c9632807365
Rumen Yankov <rumen.yankov@gmail.com>
2025-01-03 16:56:00 +0200

    fixes

--- src/TemplateProcessors/WebVella.Tefter.TemplateProcessors.ExcelFile/Models/ExcelFileTemplateResultItem.cs
+++ src/TemplateProcessors/WebVella.Tefter.TemplateProcessors.ExcelFile/Models/ExcelFileTemplateResultItem.cs
@@ -4,4 +4,5 @@ public class ExcelFileTemplateResultItem
 {
 	public string FileName { get; set; }
 	public byte[] Content { get; set; }
+	public List<ValidationError> Errors { get; set; } = new();
 }
--- src/TemplateProcessors/WebVella.Tefter.TemplateProcessors.TextFile/Models/TextFileTemplateResultItem.cs
+++ src/TemplateProcessors/WebVella.Tefter.TemplateProcessors.TextFile/Models/TextFileTemplateResultItem.cs
@@ -4,4 +4,6 @@ public class TextFileTemplateResultItem
 {
 	public string FileName { get; set; }
 	public byte[] Content { get; set; }
+
+	public List<ValidationError> Errors { get; set; } = new();
 }
