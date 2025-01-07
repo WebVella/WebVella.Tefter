e98a2b839ccf4a27e7066e6ce83c0f0d2922eb45
Bozhidar Zashev <bzashev@users.noreply.github.com>
2025-01-07 15:34:01 +0200

    subscribe

--- src/TemplateProcessors/WebVella.Tefter.TemplateProcessors.ExcelFile/Component/ResultPreview/ResultPreviewComponent.razor.cs
+++ src/TemplateProcessors/WebVella.Tefter.TemplateProcessors.ExcelFile/Component/ResultPreview/ResultPreviewComponent.razor.cs
@@ -5,6 +5,7 @@ namespace WebVella.Tefter.TemplateProcessors.ExcelFile.Components;
 [LocalizationResource("WebVella.Tefter.TemplateProcessors.ExcelFile.Components.ResultPreview.ResultPreviewComponent", "WebVella.Tefter.TemplateProcessors.ExcelFile")]
 public partial class ResultPreviewComponent : TfFormBaseComponent, ITfCustomComponent
 {
+	[Inject] private IServiceProvider ServiceProvider { get; set; }
 	//For this component only ReadOnly and Form will be supported
 	[Parameter] public TfComponentMode DisplayMode { get; set; } = TfComponentMode.Read;
 	[Parameter] public string Value { get; set; }
