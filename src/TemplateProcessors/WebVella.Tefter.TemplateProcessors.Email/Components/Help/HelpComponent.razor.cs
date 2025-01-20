4c3a8cd0dc4abd5c96ac8bcce71b166e71ecc5f5
Bozhidar Zashev <bzashev@users.noreply.github.com>
2025-01-20 14:21:48 +0200

    email help

--- src/TemplateProcessors/WebVella.Tefter.TemplateProcessors.Email/Components/Help/HelpComponent.razor.cs
+++ src/TemplateProcessors/WebVella.Tefter.TemplateProcessors.Email/Components/Help/HelpComponent.razor.cs
@@ -3,33 +3,11 @@
 namespace WebVella.Tefter.TemplateProcessors.Email.Components;
 
 [LocalizationResource("WebVella.Tefter.TemplateProcessors.Email.Components.Help.HelpComponent", "WebVella.Tefter.TemplateProcessors.Email")]
-public partial class HelpComponent : TfFormBaseComponent, ITfDynamicComponent<TfTemplateProcessorHelpComponentContext>
+public partial class HelpComponent : TfBaseComponent, ITfDynamicComponent<TfTemplateProcessorHelpComponentContext>
 {
 	//For this component only ReadOnly and Form will be supported
 	[Parameter] public TfComponentMode DisplayMode { get; set; } = TfComponentMode.Read;
 	[Parameter] public TfTemplateProcessorHelpComponentContext Context { get; set; }
-
-	private EmailTemplateSettings _form = new();
-
-
-	protected override void OnInitialized()
-	{
-		base.OnInitialized();
-		base.InitForm(_form);
-	}
-
-
-
-	public List<ValidationError> Validate()
-	{
-		MessageStore.Clear();
-		var errors = new List<ValidationError>();
-
-
-		var isValid = EditContext.Validate();
-		StateHasChanged();
-		return errors;
-	}
 
 
 }
