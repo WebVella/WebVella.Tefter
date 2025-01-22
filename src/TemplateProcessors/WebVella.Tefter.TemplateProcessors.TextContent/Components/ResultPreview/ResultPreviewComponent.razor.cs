b256d20dbc9098b324fb507ab54ec093fac36af0
Bozhidar Zashev <bzashev@users.noreply.github.com>
2025-01-22 16:25:37 +0200

    dev

--- src/TemplateProcessors/WebVella.Tefter.TemplateProcessors.TextContent/Components/ResultPreview/ResultPreviewComponent.razor.cs
+++ src/TemplateProcessors/WebVella.Tefter.TemplateProcessors.TextContent/Components/ResultPreview/ResultPreviewComponent.razor.cs
@@ -3,33 +3,50 @@
 namespace WebVella.Tefter.TemplateProcessors.TextContent.Components;
 
 [LocalizationResource("WebVella.Tefter.TemplateProcessors.TextContent.Components.ResultPreview.ResultPreviewComponent", "WebVella.Tefter.TemplateProcessors.TextContent")]
-public partial class ResultPreviewComponent : TfFormBaseComponent, ITfDynamicComponent<TfTemplateProcessorResultPreviewComponentContext>
+public partial class ResultPreviewComponent : TfBaseComponent, ITfDynamicComponent<TfTemplateProcessorResultPreviewComponentContext>
 {
-	//For this component only ReadOnly and Form will be supported
+	[Inject] private ITfTemplateService TemplateService { get; set; }
 	[Parameter] public TfComponentMode DisplayMode { get; set; } = TfComponentMode.Read;
 	[Parameter] public TfTemplateProcessorResultPreviewComponentContext Context { get; set; }
 
-	private TextContentTemplateSettings _form = new();
-
-
+	private TextContentTemplatePreviewResult _preview = null;
+	private bool _isLoading = true;
+	private List<ValidationError> _previewValidationErrors = new();
+	private int _itemPosition = 1;
 	protected override void OnInitialized()
 	{
 		base.OnInitialized();
-		base.InitForm(_form);
+		if (Context is null) throw new Exception("Context is not defined");
+		Context.ValidatePreviewResult = _validatePreviewResult;
 	}
-
-
-
-	public List<ValidationError> Validate()
+	protected override async Task OnAfterRenderAsync(bool firstRender)
 	{
-		MessageStore.Clear();
-		var errors = new List<ValidationError>();
+		await base.OnAfterRenderAsync(firstRender);
+		if (firstRender)
+		{
+			if (Context.Template is not null && Context.SpaceData is not null)
+			{
+				ITfTemplatePreviewResult result = TemplateService.GenerateTemplatePreviewResult(
+					templateId: Context.Template.Id,
+					spaceDataId: Context.SpaceData.Id,
+					tfRecordIds: Context.SelectedRowIds
+				);
+				if (result is not TextContentTemplatePreviewResult)
+				{
+					throw new Exception("Preview result is not of type TextContentTemplatePreviewResult");
+				}
+				_preview = (TextContentTemplatePreviewResult)result;
+				await Context.PreviewResultChanged.InvokeAsync(_preview);
+			}
 
-
-		var isValid = EditContext.Validate();
-		StateHasChanged();
-		return errors;
+			_isLoading = false;
+			StateHasChanged();
+		}
 	}
-
+	private List<ValidationError> _validatePreviewResult()
+	{
+		_previewValidationErrors = new List<ValidationError>();
+		return _previewValidationErrors;
+	}
 
 }
