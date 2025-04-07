b87be85274472a1eec30b9b645224b4abc44840c
Bozhidar Zashev <bzashev@users.noreply.github.com>
2025-04-07 17:43:56 +0300

    test fix

--- src/WebVella.Tefter/Web/Components/General/ValidationMessage/TfValidationMessage.razor.cs
+++ src/WebVella.Tefter/Web/Components/General/ValidationMessage/TfValidationMessage.razor.cs
@@ -2,7 +2,7 @@
 public partial class TfValidationMessage : ComponentBase
 {
 	[Parameter] public string Field { get; set; }
-	[Parameter] public long Index { get; set; } = -5;
+	[Parameter] public long Index { get; set; } = -1;
 	[Parameter] public List<ValidationError> Errors { get; set; }
 	[Parameter] public string Class { get; set; }
 	[Parameter] public string Style { get; set; }
@@ -13,9 +13,6 @@ public partial class TfValidationMessage : ComponentBase
 	{
 		get
 		{
-			if(Field == "DefaultValue" && Index >= 0){ 
-				var boz = 0;
-			}
 			var list = (Errors ?? new List<ValidationError>()).Where(x => x.PropertyName == Field && x.Index == Index).ToList();
 			return list;
 		}
