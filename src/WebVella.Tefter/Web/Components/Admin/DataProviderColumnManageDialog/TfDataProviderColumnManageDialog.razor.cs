5aa093c3525d3b9a2c3543c32947ff1243467d09
Bozhidar Zashev <bzashev@users.noreply.github.com>
2025-04-17 15:32:24 +0300

    data base column prefix

--- src/WebVella.Tefter/Web/Components/Admin/DataProviderColumnManageDialog/TfDataProviderColumnManageDialog.razor.cs
+++ src/WebVella.Tefter/Web/Components/Admin/DataProviderColumnManageDialog/TfDataProviderColumnManageDialog.razor.cs
@@ -145,6 +145,7 @@ public partial class TfDataProviderColumnManageDialog : TfFormBaseComponent, IDi
 				SourceType = _selectedProviderType.Name,
 				DbType = _selectedDbType
 			};
+			_form.DbName = TfAppState.Value.AdminDataProvider.ColumnPrefix + _form.DbName;
 			TucDataProvider provider;
 			if (_isCreate)
 			{
