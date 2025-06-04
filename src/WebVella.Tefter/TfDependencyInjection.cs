e94cb6d71774dc529b4d96e9611d0852880e949f
Rumen Yankov <rumen.yankov@gmail.com>
2025-06-04 14:28:35 +0300

    updates data provider synchronization to reflect changes added with data identity support

--- src/WebVella.Tefter/TfDependencyInjection.cs
+++ src/WebVella.Tefter/TfDependencyInjection.cs
@@ -75,7 +75,6 @@ public static class TfDependencyInjection
 		//hosted services
 		services.AddHostedService<TfDataProviderSynchronizeJob>();
 		services.AddHostedService<TfBlobMaintenanceJob>();
-		services.AddHostedService<TfJoinKeysUpdateJob>();
 		services.AddHostedService<TfIdsCacheLoaderJob>();
 		services.AddHostedService<TfDataProviderSynchScheduleJob>();
 		
