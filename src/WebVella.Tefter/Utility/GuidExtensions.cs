95e05871642773619cd659e0586505d3b5b38baa
Rumen Yankov <rumen.yankov@gmail.com>
2025-06-19 12:30:05 +0300

    adds guid extenstions

---
+++ src/WebVella.Tefter/Utility/GuidExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace WebVella.Tefter.Utility;
+
+public static class GuidExtensions
+{
+	static string ToSha1(this Guid? guid)
+	{
+		using (SHA1 sha1 = SHA1.Create())
+		{
+			string input = guid.HasValue ? guid.Value.ToString() : string.Empty;
+			byte[] hashBytes = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+			StringBuilder sb = new StringBuilder();
+			foreach (byte b in hashBytes)
+			{
+				sb.Append(b.ToString("x2")); // Convert to hexadecimal
+			}
+			return sb.ToString();
+		}
+	}
+
+	public static string ToSha1(this Guid guid)
+	{
+		using (SHA1 sha1 = SHA1.Create())
+		{
+			string input = guid.ToString();
+			byte[] hashBytes = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+			StringBuilder sb = new StringBuilder();
+			foreach (byte b in hashBytes)
+			{
+				sb.Append(b.ToString("x2")); // Convert to hexadecimal
+			}
+			return sb.ToString();
+		}
+	}
+}
