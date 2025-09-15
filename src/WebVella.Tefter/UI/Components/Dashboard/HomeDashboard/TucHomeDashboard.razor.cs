3c971623f3f4cc9a321f44a615a8a956cac9132e
Bozhidar Zashev <bzashev@users.noreply.github.com>
2025-09-15 12:26:07 +0300

    dev

--- src/WebVella.Tefter/UI/Components/Dashboard/HomeDashboard/TucHomeDashboard.razor.cs
+++ src/WebVella.Tefter/UI/Components/Dashboard/HomeDashboard/TucHomeDashboard.razor.cs
@@ -1,5 +1,6 @@
 ﻿
 namespace WebVella.Tefter.UI.Components;
+
 [LocalizationResource("WebVella.Tefter.UI.Components.Dashboard.HomeDashboard.TucHomeDashboard", "WebVella.Tefter")]
 public partial class TucHomeDashboard : TfBaseComponent
 {
@@ -8,6 +9,8 @@ public partial class TucHomeDashboard : TfBaseComponent
 	[Inject] protected ITfNavigationUIService ITfNavigationUIService { get; set; } = default!;
 
 	private TfHomeDashboardData _data = new();
+	private List<TfHowToItem> _howToItems = new();
+
 
 	protected override async Task OnInitializedAsync()
 	{
@@ -15,6 +18,39 @@ public partial class TucHomeDashboard : TfBaseComponent
 		var user = await TfUserUIService.GetCurrentUserAsync();
 		if (user is not null)
 			_data = TfDashboardUIService.GetHomeDashboardData(user.Id);
+
+		_howToItems.Add(new TfHowToItem
+		{
+			Title = "Create a data provider",
+			Description = "The data provider defines the source, import process, storage structure, and synchronization rules for maintaining up-to-date data.",
+			MoreInfoUrl = "https://tefter.webvella.com/en/docs/administrators/data-providers",
+			FluentIconName = _data.ProvidersCount > 0 ? "Checkmark" : null,
+			Count = _data.ProvidersCount > 0 ? null : 1,
+			BackgroundColor = _data.ProvidersCount > 0 ? TfColor.Green100.GetAttribute().Value : null,
+			Color = _data.ProvidersCount > 0 ? TfColor.Green700.GetAttribute().Value : null,
+		});
+
+		_howToItems.Add(new TfHowToItem
+		{
+			Title = "Create a space",
+			Description = "The Space is a user interface area that organizes pages with a common purpose or those intended for use by a specific team.",
+			MoreInfoUrl = "https://tefter.webvella.com/en/docs/end-users/space",
+			FluentIconName = _data.SpacesCount > 0 ? "Checkmark" : null,
+			Count = _data.SpacesCount > 0 ? null : 2,
+			BackgroundColor = _data.SpacesCount > 0 ? TfColor.Green100.GetAttribute().Value : null,
+			Color = _data.SpacesCount > 0 ? TfColor.Green700.GetAttribute().Value : null,
+		});
+
+		_howToItems.Add(new TfHowToItem
+		{
+			Title = "Create a 'Space view' page",
+			Description = "The Space View presents data to users and provides core data collaboration features. It determines the display format for specific data columns, allows for quick filter setup.",
+			MoreInfoUrl = "https://tefter.webvella.com/en/docs/end-users/space-page",
+			FluentIconName = _data.SpacePagesCount > 0 ? "Checkmark" : null,
+			Count = _data.SpacePagesCount > 0 ? null : 3,
+			BackgroundColor = _data.SpacePagesCount > 0 ? TfColor.Green100.GetAttribute().Value : null,
+			Color = _data.SpacePagesCount > 0 ? TfColor.Green700.GetAttribute().Value : null,
+		});
 	}
 
 	private async Task _onSearch(string value)
@@ -40,7 +76,7 @@ public partial class TucHomeDashboard : TfBaseComponent
 
 	private void _onClick(TfSearchResult result)
 	{
-		if(!String.IsNullOrWhiteSpace(result.Url))
+		if (!String.IsNullOrWhiteSpace(result.Url))
 			Navigator.NavigateTo(result.Url);
 	}
 
---
+++ src/WebVella.Tefter/UIModels/TfHowToItem.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebVella.Tefter.Models;
+
+public record TfHowToItem
+{
+	public string? Title { get; set; }
+	public string? Description { get; set; }
+	public string? FluentIconName { get; set; }
+	public string? Color { get; set; }
+	public string? BackgroundColor { get; set; }
+	public string? MoreInfoUrl { get; set; }
+	public int? Count { get; set; } = null;
+	public string IconStyle
+	{
+		get
+		{
+			var sb = new StringBuilder();
+
+			if (!String.IsNullOrWhiteSpace(Color))
+				sb.Append($"color: {Color}; --accent-fill-rest: {Color};");
+
+			if (String.IsNullOrWhiteSpace(BackgroundColor))
+				sb.Append($"background-color: var(--tf-indigo-100);");
+			else
+				sb.Append($"background-color: {BackgroundColor};");
+
+			return sb.ToString();
+		}
+	}
+}
